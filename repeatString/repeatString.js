bda68538cac770d7dc0414b13b34f86719edf6f3
green7 <sohnf90@yahoo.com>
2020-09-20 22:36:48 -0700

    Completed repeatString function and tests are successful

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let finStr = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        finStr += str;
+    }
+    return finStr;
 }
 
 module.exports = repeatString
