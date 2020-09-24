3c68e83d02b7d5e45bcb5aa5a2bfbde2563f08f7
green7 <sohnf90@yahoo.com>
2020-09-24 00:35:33 -0700

    Completed sumAll. Passed all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    if(typeof arg1 != "number" || typeof arg2 != "number" || arg1 < 0 || arg2 < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    if(arg1 < arg2){
+        for(let i = arg1; i <= arg2; i++){
+            sum += i;
+        }
+    }
+    else{
+        for(let i = arg2; i <= arg1; i++){
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 module.exports = sumAll
