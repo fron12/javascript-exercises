f1bc25af32d78441c7efa2d6166c3bc83ec99c9b
green7 <sohnf90@yahoo.com>
2020-09-24 01:24:04 -0700

    Completed tempConversion. Passed all tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let celcius = ((num - 32) * 5) / 9;
+  if(celcius !== 0){
+    return parseFloat(celcius.toFixed(1));
+  }
+  else{
+    return celcius;
+  }
 }
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let fahrenheit = ((num / 5) * 9) + 32;
+  if(fahrenheit !== 0){
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  else{
+    return fahrenheit;
+  }
 }
 
 module.exports = {
