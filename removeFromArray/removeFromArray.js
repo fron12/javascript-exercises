1dc02592922527898838727358879caa7f800345
green7 <sohnf90@yahoo.com>
2020-09-24 00:10:50 -0700

    Completed removefromArray using nested for loop. Passed tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rem) {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < rem.length; j++){
+            if(arr[i] === rem[j]){
+                arr.splice(i, 1);
+                rem.splice(j, 1);
+                removeFromArray(arr, ...rem);
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
