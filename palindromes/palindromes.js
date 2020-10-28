b892b3346029b5f37a3d084cabbee1087cfaf745
green7 <sohnf90@yahoo.com>
2020-10-27 23:43:00 -0700

    Completed palindromes with all tests passing

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    let strChars = str.split("");
+    let onlyLetters = strChars.filter(function(letter){
+        return punctuation.indexOf(letter) === -1;
+    }) 
+    let originalStringJoined = onlyLetters.join("").split(" ").join("").toLowerCase();
+    let reversedStringJoined = onlyLetters.reverse().join("").split(" ").join("").toLowerCase();
 
+    if(originalStringJoined === reversedStringJoined){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-
 module.exports = palindromes
