0878c2891ecc2e960243b529a1ce55ad3e6160a7
green7 <sohnf90@yahoo.com>
2020-10-27 23:44:07 -0700

    Completed factorial with tests passing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -22,8 +22,15 @@ function power(num1, num2) {
 	return Math.pow(num1, num2);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num === 0) {
+		return 1;
+	}
+	let total = 1;
+	for(let i = num; i > 0; i--){
+		total *= i;
+	}
+	return total;
 }
 
 module.exports = {
