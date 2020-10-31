a65133ac75d65b24b692f0e20cdbc9b46ba97e22
green7 <sohnf90@yahoo.com>
2020-10-31 12:46:27 -0700

    Completed getTheTitles. Test passes

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    // From the array of objects, return an array of the objects' titles
+    return arr.map(book => book.title);
 }
 
 module.exports = getTheTitles;
